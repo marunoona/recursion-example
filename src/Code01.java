2b5f7ac8568b215618e68b02da2ba6e14edcd9aa
marunoona <ye_jin92@naver.com>
2019-03-01 15:09:40 +0900

    first commit

---
+++ src/Code01.java
@@ -0,0 +1,69 @@
+public class Code01 {
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println("#1. "+n +"까지의 합 :"+func(n));
+        System.out.println("#2. "+n+"! = "+factorial(n));
+        System.out.println("#3. "+n+"의 3제곱"+power(4,3));
+        System.out.println("#4. "+n+"의 fibonacci 값 : "+fibonacci(n));
+        System.out.println("#5. "+"9와 3의 최대공약수 : " + gcd(9,3));
+        System.out.println("#6. "+"7과 14의 최대공약수 : " + gcd2(7,14));
+    }
+
+    private static int func(int n){
+        if(n == 0)
+            return 0;
+        else
+            return n + func(n - 1);
+    }
+
+    private static int factorial(int n){
+        if(n==0){
+            return 1;
+        }else
+            return n*factorial(n-1);
+    }
+
+    private static double power(double x, int n){
+        if(n==0){
+            return 1;
+        }else{
+            return x*power(x,n-1);
+        }
+    }
+
+    private static int fibonacci(int n){
+        if(n < 2){
+            return n;
+        }else{
+            return fibonacci(n-1) + fibonacci(n -2);
+        }
+    }
+
+    /**
+     * 최대공약수
+     * Euclid Method
+     * @param m
+     * @param n
+     * @return
+     */
+    private static int gcd(int m, int n){
+        if (m < n) {
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        if (m % n == 0) {
+            return n;
+        } else {
+            return gcd(n, m % n);
+        }
+    }
+
+    private static int gcd2(int p, int q) {
+        if (q == 0)
+            return p;
+        else
+            return gcd2(q, p % q);
+    }
+}
