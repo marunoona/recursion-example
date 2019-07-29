e09c3b1e140d3bb0a2a331d747242efee04ff4c5
marunoona <ye_jin92@naver.com>
2019-07-29 21:29:26 +0900

    수정

--- src/Code01.java
+++ src/Code01.java
@@ -1,53 +1,68 @@
 public class Code01 {
+    private static int[] table = new int[32];
 
     public static void main(String[] args) {
         int n = 4;
-        System.out.println("#1. "+n +"까지의 합 :"+func(n));
-        System.out.println("#2. "+n+"! = "+factorial(n));
-        System.out.println("#3. "+n+"의 3제곱"+power(4,3));
-        System.out.println("#4. "+n+"의 fibonacci 값 : "+fibonacci(n));
-        System.out.println("#5. "+"9와 3의 최대공약수 : " + gcd(9,3));
-        System.out.println("#6. "+"7과 14의 최대공약수 : " + gcd2(7,14));
+        System.out.println("#1. " + n + "까지의 합 :" + func(n));
+        System.out.println("#2. " + n + "! = " + factorial(n));
+        System.out.println("#3. " + n + "의 3제곱" + power(4, 3));
+        System.out.println("#4. fibonacci(4) 값 : " + fibonacci(4));
+        System.out.println("#5. memofibo(4) 값 : " + memoFibo(4));
+        System.out.println("#6. " + "9와 3의 최대공약수 : " + gcd(9, 3));
+        System.out.println("#7. " + "7과 14의 최대공약수 : " + gcd2(7, 14));
     }
 
-    private static int func(int n){
-        if(n == 0)
+    private static int func(int n) {
+        if (n == 0)
             return 0;
         else
             return n + func(n - 1);
     }
 
-    private static int factorial(int n){
-        if(n==0){
+    private static int factorial(int n) {
+        if (n == 0) {
             return 1;
-        }else
-            return n*factorial(n-1);
+        } else
+            return n * factorial(n - 1);
     }
 
-    private static double power(double x, int n){
-        if(n==0){
+    private static double power(double x, int n) {
+        if (n == 0) {
             return 1;
-        }else{
-            return x*power(x,n-1);
+        } else {
+            return x * power(x, n - 1);
         }
     }
 
-    private static int fibonacci(int n){
-        if(n < 2){
+    private static int fibonacci(int n) {
+        if (n < 2) {
             return n;
-        }else{
-            return fibonacci(n-1) + fibonacci(n -2);
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
+    }
+
+    private static int memoFibo(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (table[n] > 0) {
+            return table[n];
+        }
+        table[n] = memoFibo(n - 1) + memoFibo(n - 2);
+        System.out.println(table[n]);
+        return table[n];
     }
 
     /**
      * 최대공약수
      * Euclid Method
+     *
      * @param m
      * @param n
      * @return
      */
-    private static int gcd(int m, int n){
+    private static int gcd(int m, int n) {
         if (m < n) {
             int tmp = m;
             m = n;
--- src/Code02.java
+++ src/Code02.java
@@ -1,5 +1,64 @@
 public class Code02 {
     public static void main(String[] args) {
+        String string = "marunoona";
+        System.out.println(string + "의 길이:" + length(string));
+        printCharsReverse(string);
+        System.out.println();
+        printBinary(6);
+    }
 
+    /**
+     * 문자열 길이 계산
+     *
+     * @param string 문자열
+     * @return 문자열 길이
+     */
+    private static int length(String string) {
+        if (string.equals(""))
+            return 0;
+        else
+            return 1 + length(string.substring(1));
+    }
+
+    /**
+     * 문자열 뒤집어서 프린트
+     *
+     * @param str 문자열
+     */
+    private static void printCharsReverse(String str) {
+        if (str.length() == 0) {
+            return;
+        } else {
+            printCharsReverse(str.substring(1));
+            System.out.print(str.charAt(0));
+        }
+    }
+
+    /**
+     * 2진수 구하기
+     *
+     * @param n
+     */
+    private static void printBinary(int n) {
+        if (n < 2)
+            System.out.print(n);
+        else {
+            printBinary(n / 2);
+            System.out.print(n % 2);
+        }
+    }
+
+    /**
+     * 배열의 합 구하기
+     *
+     * @param n
+     * @param data
+     * @return
+     */
+    private static int sum(int n, int[] data) {
+        if (n <= 0)
+            return 0;
+        else
+            return sum(n - 1, data) + data[n - 1];
     }
 }
